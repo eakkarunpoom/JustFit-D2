a34f907f635e09b968db457a7d80e7da234f591f
Chanatinart K <125489141+chanatinart02@users.noreply.github.com>
2023-05-05 08:53:43 +0700

    add animation in login signUp btn

--- src/component/LoginBtn/LoginBtn.jsx
+++ src/component/LoginBtn/LoginBtn.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./LoginBtn.css";
 
 function LoginBtn() {
   return (
-    <a className="btn botton login" href="/Login">
+    <motion.a
+      className="btn botton login"
+      href="/Login"
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+    >
       Login
-    </a>
+    </motion.a>
   );
 }
 
