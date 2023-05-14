93a2e78778abdffae25086b1344e1d1c34bc7bf6
WUTTICHAI <mr.wuttichai.ktsm@gmail.com>
2023-05-14 23:16:23 +0700

    add-profile-page

--- ProfilePage.jsx
+++ ProfilePage.jsx
@@ -1,44 +1,55 @@
 import React from "react";
 import Layout from "../../component/Layout";
 import { ProfileImg } from "../../assets";
+import "./ProfilePage.css";
 
 const ProfilePage = () => {
     return (
         <Layout>
-            <mian>
-                <section>
-                    <article>
-                        <h1>My Profile</h1>
-                        <p>Current Photo</p>
-                        <img src={ProfileImg} alt="Profile Photo" />
+            <main>
+                <section className="profile">
+                    <article className="head">
+                        <div className="title">
+                             <h1>My Profile</h1>
+                        </div>
+                        <div className="current-photo">
+                            <p>Current Photo</p>
+                        </div>
+                        <div className="profileimg">
+                            <img src={ProfileImg} alt="Profile Photo" />
+                        </div>
                     </article>
-                    <table>
-                        <tr>
-                            <td>Name:</td>
-                            <td>Jessica Jusfit</td>
-                        </tr>
-                        <tr>
-                            <td>Gender:</td> 
-                            <td>Female</td>
-                        </tr>
-                        <tr>
-                            <td>DOB:</td>
-                            <td>12/25/2000</td>
-                        </tr>
-                        <tr>
-                            <td>Height:</td>
-                            <td>167</td>
-                        </tr>
-                        <tr>
-                            <td>Weight:</td>
-                            <td>47</td>
-                        </tr>
-                    </table>
-                    <button>
-                        <a href="/">Edit Profile</a>
-                    </button>
+                    <div className="detail">
+                        <table>
+                            <tr>
+                                <td>Name:</td>
+                                <td>Jessica Jusfit</td>
+                            </tr>
+                            <tr>
+                                <td>Gender:</td> 
+                                <td>Female</td>
+                            </tr>
+                            <tr>
+                                <td>DOB:</td>
+                                <td>12/25/2000</td>
+                            </tr>
+                            <tr>
+                                <td>Height:</td>
+                                <td>167</td>
+                            </tr>
+                            <tr>
+                                <td>Weight:</td>
+                                <td>47</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div className="btn-profile">
+                        <button className="edit-profile">
+                            <a href="/">Edit Profile</a>
+                        </button>
+                    </div>
                 </section>
-            </mian>
+            </main>
         </Layout>
     )
 };
