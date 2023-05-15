debb5c1f8535fadf0a2357e85fdfcf5a531cee1e
Chanatinart K <125489141+chanatinart02@users.noreply.github.com>
2023-05-16 02:08:28 +0700

    responsive navbar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,42 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./Header.css";
 import LoginBtn from "../LoginBtn/LoginBtn";
 import SignUpBtn from "../SingUpBtn/SignUpBtn";
 
 const Header = () => {
   return (
-    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom sticky-top">
-      <div className="col-md-3 mb-2 mb-md-0">
-        <a
-          href="/"
-          className="d-inline-flex link-body-emphasis text-decoration-none logo"
-        >
-          Just<span>Fit</span>
-        </a>
-      </div>
-
-      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li>
-          <a href="/workOut" className="nav-link px-2">
-            Workout Videos
-          </a>
-        </li>
-        <li>
-          <a href="/recipes" className="nav-link px-2">
-            Recipes
-          </a>
-        </li>
-        <li>
-          <a href="/dashBoard" className="nav-link px-2">
-            Dash board
-          </a>
-        </li>
-      </ul>
-
-      <div className="col-md-3 text-end">
-        <LoginBtn />
-        <SignUpBtn variant="primary" />
-      </div>
-    </header>
+    <div className="navbar-container">
+      <Navbar className="header sticky-top" expand="lg">
+        <Container>
+          <Navbar.Brand href="/" className="logo">
+            Just<span>Fit</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-center flex-grow-1 pe-3">
+              <Nav.Link href="/workOut">Workout Videos</Nav.Link>
+              <Nav.Link href="/recipes">Recipes</Nav.Link>
+              <Nav.Link href="/dashBoard">Dash board</Nav.Link>
+            </Nav>
+            <Nav className="collapse-btn">
+              <LoginBtn />
+              <SignUpBtn variant="primary" />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
